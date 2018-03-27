659a4f3ef1b10367f4eb04c0eaee535d8ff67097
Cherry Xu <cherryxuqianyun@gmail.com>
2018-03-26 21:50:22 -0700

    refactored header to stateless component

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -4,18 +4,14 @@ import HeaderLink from './HeaderLink';
 import H1 from '../H1/index';
 
 
-class Header extends React.Component { // eslint-disable-line react/prefer-stateless-function
-  render() {
-    return (
-      <div>
-        <H1>{"CHERRY'S NOTEPAD"}</H1>
-        <NavBar>
-          <HeaderLink to="/">Home</HeaderLink>
-          <HeaderLink to="/notes">Notes</HeaderLink>
-        </NavBar>
-      </div>
-    );
-  }
-}
+const Header = () => (
+  <div>
+    <H1>{"CHERRY'S NOTEPAD"}</H1>
+    <NavBar>
+      <HeaderLink to="/">Home</HeaderLink>
+      <HeaderLink to="/notes">Notes</HeaderLink>
+    </NavBar>
+  </div>
+);
 
 export default Header;
